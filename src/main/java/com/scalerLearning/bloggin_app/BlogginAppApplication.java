d13aa31373efbcf9eb18b9b97622615d9108161d
Jagadesh Narayanaswamy <javajagz24@gmail.com>
2024-01-31 19:39:10 +0000

    Initial commit

---
+++ src/main/java/com/scalerLearning/bloggin_app/BlogginAppApplication.java
@@ -0,0 +1,13 @@
+package com.scalerLearning.bloggin_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogginAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogginAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scalerLearning/bloggin_app/BlogginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalerLearning.bloggin_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
